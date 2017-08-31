d598752faff7a579110b66168fe8160d9c9d58b2
Lexi <lexithestrawhat@gmail.com>
2017-08-30 21:02:46 -0400

    Added card card randomizer (tarot()) in prompt mode

--- src/promptMe.py
+++ src/promptMe.py
@@ -11,18 +11,25 @@ import random
 
 names = ("Jack", "Lana", "Scott", "Mary", "Wren", "Alice",
                  "Elijah", "Claire", "Silas", "Emily", "Teddy", "Sam")
-settings = ("Steampunk", "Witcher")
+
+
+def tarot() -> str:
+    card_number = random.randint(1, 78)
+    if card_number > 56:
+        card_name = "Major"
+    else:
+        card_name = "Minor"
+    return card_name
 
 
 def main():
     random.seed(None)
-    auto = input("Run in manual mode? ").lower()
+    auto = input("Run in universe mode? ").lower()
     if auto == "y" or auto == "yes":
         characters.manual()
     else:
         print("Focus character: " + names[random.randint(0, len(names)-1)])
-        print("Setting: " + settings[random.randint(0, len(settings)-1)])
-
+        print("")
 
 
 main()
